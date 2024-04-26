328a5ebc468651d11de5cc455bc7168e8c1019c3
kamalnkv <93749384+kamalnkv@users.noreply.github.com>
2024-04-26 18:26:04 +0530

    added new line for check push to jenkins

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Kamal here is your second Java Program!"); 
+        System.out.println("Hello, Git hub push test"); 
     }
 }
