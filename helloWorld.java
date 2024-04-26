f9f2548a41db48b3c791e475ce2756fa9426121f
kamalnkv <93749384+kamalnkv@users.noreply.github.com>
2024-04-26 18:28:03 +0530

    Second line check push to jenkins

--- helloWorld.java
+++ helloWorld.java
@@ -2,5 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Kamal here is your second Java Program!"); 
         System.out.println("Hello, Git hub push test"); 
+        System.out.println("Hello, second time check"); 
+        
     }
 }
